6a1cc519ea64035232a788ea802d579b6b5cb91e
JunianDan <161720171+JunianDan@users.noreply.github.com>
2024-02-29 22:48:20 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1709221399,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/171406one_punch_man_dpsaitamaone_punch_mananimesleeve550x-sheet0.png",
+		"images/istockphoto1253231143170667a-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/bawah-sheet0.png",
+		"images/atas-sheet0.png",
+		"images/freehdconvertcomistockphoto1253231143170667a-sheet0.png",
+		"images/freehdconvertcomistockphoto1253231143170667a2-sheet0.png",
+		"images/whatsappimage20240215at-sheet0.png",
+		"images/whatsappimage20240215at-sheet1.png",
+		"images/whatsappimage20240215at-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
